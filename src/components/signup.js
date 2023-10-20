721ca25c1d53766da8ee69ee2ddad30027d67907
NoorAshna <noorahmedspider@gmail.com>
2023-10-20 07:50:55 +0500

    integrate 1

--- src/components/signup.js
+++ src/components/signup.js
@@ -2,6 +2,41 @@ import React from 'react';
 import './Signup.css';
 import mainlogo from "../components/images/logomain.png";
 export default function Signup() {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:5000/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.status === 201) {
+        const data = await response.json();
+        console.log(data);
+      } else {
+        const errorData = await response.json();
+        console.error(errorData);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
   return (
     <div className='MainBody'>
     <div className='logo'>
@@ -14,7 +49,8 @@ export default function Signup() {
       <form action="#" method="post">
         <div className="input-field">
           <label htmlFor="username" className="input-label">Username</label>
-          <input type="text" id="username" className="input" />
+          <input type="text" id="username" className="input" value={formData.username}
+            onChange={handleInputChange}/>
         </div>
         <div className="input-field">
           <label htmlFor="email" className="input-label">Email</label>
@@ -22,13 +58,15 @@ export default function Signup() {
         </div>
         <div className="input-field">
           <label htmlFor="password" className="input-label">Password</label>
-          <input type="password" id="password" className="input" />
+          <input type="password" id="password" className="input" value={formData.password}
+            onChange={handleInputChange}/>
         </div>
         <div className="input-field">
           <label htmlFor="confirm-password" className="input-label">Confirm Password</label>
-          <input type="password" id="confirm-password" className="input" />
+          <input type="password" id="confirm-password" className="input" value={formData.confirmPassword}
+            onChange={handleInputChange}/>
         </div>
-        <button type="submit" className="submit-button">Sign Up</button>
+        <button type="submit" className="submit-button" onSubmit={handleSubmit}>Sign Up</button>
       </form>
 
       <div className='triangle'></div>
